78ffea99df037a0d96f82f90f505fe6f481d1c5c
Up-s <dldbdjq@gmail.com>
2024-05-16 15:03:33 +0900

    USE env config

--- gps_map/ios/Runner/AppDelegate.swift
+++ gps_map/ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import flutter_config
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,7 +10,11 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    GMSServices.provideAPIKey("")
+    
+    if let googleMapKey = FlutterConfigPlugin.env(for: "google_map_key") {
+      GMSServices.provideAPIKey(googleMapKey)
+    }
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
